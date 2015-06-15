6141244bf39d56db44710afb860d94574fa132b5
Yuriy_Getsko <getttzzz@gmail.com>
2015-06-15 12:31:33 +0300

    Add button "Alarm"

---
+++ app/src/main/java/chisw/com/plans/ui/AlarmActivity.java
@@ -0,0 +1,19 @@
+package chisw.com.plans.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import chisw.com.plans.R;
+
+/**
+ * Created by Yuriy on 15.06.2015.
+ */
+public class AlarmActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_main );
+    }
+}
