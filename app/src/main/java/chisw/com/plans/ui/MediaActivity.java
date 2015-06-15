ff1b15a05563d9e2efe2e6ee9139e2fc50e0dff6
Konstantin <yamkostya@gmail.com>
2015-06-15 18:00:30 +0300

    Event handler for Go back btn on Media activity added

--- app/src/main/java/chisw/com/plans/ui/MediaActivity.java
+++ app/src/main/java/chisw/com/plans/ui/MediaActivity.java
@@ -3,23 +3,32 @@ package chisw.com.plans.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import chisw.com.plans.R;
 
 public class MediaActivity extends Activity {
-    private Button goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
-        goBack = (Button) findViewById(R.id.ma_goback_btn);
+        Clicker clicker = new Clicker();
+        findViewById(R.id.ma_goback_btn).setOnClickListener(clicker);
     }
 
     public static void start(Activity activity) {
         Intent intnt = new Intent(activity, MediaActivity.class);
         activity.startActivity(intnt);
     }
-
+    public final class Clicker implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            switch (view.getId()){
+                case R.id.ma_goback_btn:
+                    MediaActivity.this.finish();
+            }
+        }
+    }
 }
