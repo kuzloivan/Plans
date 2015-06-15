6de865a91e8c15695bbdb04f7b9cdd36b0582db2
vdbo <19vladbond95@gmail.com>
2015-06-15 13:52:51 +0300

    added button and textView to NetManagmentActivity!

--- app/src/main/java/chisw/com/plans/ui/SplashActivity.java
+++ app/src/main/java/chisw/com/plans/ui/SplashActivity.java
@@ -8,11 +8,13 @@ import chisw.com.plans.R;
 
 public class SplashActivity extends Activity {
     private Button mediaBt;
+    private Button netMngtBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_main);
         mediaBt = (Button) findViewById(R.id.mediaBt);
+        netMngtBtn = (Button) findViewById(R.id.netMngtStart_btn);
     }
 
 
