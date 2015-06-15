17170ba096b3d2ec5fa8fabb480bb73b852c486a
Alyasko <yasko.alexander@gmail.com>
2015-06-15 12:38:26 +0300

    Commander commit.

---
+++ app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
@@ -0,0 +1,18 @@
+package chisw.com.plans.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import chisw.com.plans.R;
+
+/**
+ * Created by Alexander on 15.06.2015.
+ */
+public class PlannerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner);
+    }
+}
