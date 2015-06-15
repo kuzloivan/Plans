0fd5edb6ead3cfcf034fcbb24c31cea5ea6e9c31
Оксана <golovinova.o.v@gmail.com>
2015-06-15 12:33:59 +0300

    Add new button

---
+++ app/src/main/java/chisw/com/plans/ui/SettingActivity.java
@@ -0,0 +1,17 @@
+package chisw.com.plans.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import chisw.com.plans.R;
+
+/**
+ * Created by Оксана on 15.06.2015.
+ */
+public class SettingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+}
