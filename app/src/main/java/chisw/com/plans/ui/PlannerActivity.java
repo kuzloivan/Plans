2895167dd3f503e2ffa8b9a762a22d7cf089d47d
Alyasko <yasko.alexander@gmail.com>
2015-06-15 22:45:52 +0300

    Added: ability to go to the previous screen in the Planner Activity.

--- app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
+++ app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
@@ -1,24 +1,47 @@
 package chisw.com.plans.ui;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import chisw.com.plans.R;
 
 /**
  * Created by Alexander on 15.06.2015.
  */
-public class PlannerActivity extends Activity {
+public class PlannerActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+
+        Clicker clicker = new Clicker();
+
+        findViewById(R.id.pa_goBack_btn).setOnClickListener(clicker);
+
+        //getActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
     public static void start(Activity activity){
         Intent intent = new Intent(activity, PlannerActivity.class);
         activity.startActivity(intent);
+    }
+
+    public final class Clicker implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.pa_goBack_btn:
+                    Toast.makeText(getApplicationContext(), "We are gone...", Toast.LENGTH_SHORT).show();
+                    PlannerActivity.this.finish();
+                    break;
+            }
+        }
     }
 }
