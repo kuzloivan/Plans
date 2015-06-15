2e52420cf5f163f267a6a8a3beeb49c8658a60c5
vdbo <19vladbond95@gmail.com>
2015-06-15 13:00:57 +0300

    added start method to NetManagmentActivity class!

--- app/src/main/java/chisw/com/plans/ui/NetManagmentActivity.java
+++ app/src/main/java/chisw/com/plans/ui/NetManagmentActivity.java
@@ -1,6 +1,7 @@
 package chisw.com.plans.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,11 +9,16 @@ import android.view.MenuItem;
 
 import chisw.com.plans.R;
 
-public class NetManagment extends Activity {
+public class NetManagmentActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net_managment);
+    }
+
+    public static void start(Activity a) {
+        Intent intent = new Intent(a, NetManagmentActivity.class);
+        a.startActivity(intent);
     }
 }
