8dbad67efbe09ce55a4423721fa98192e1817cc4
Konstantin <yamkostya@gmail.com>
2015-06-15 13:36:24 +0300

    Added text view and button on MediaActivity

--- app/src/main/java/chisw/com/plans/ui/MediaActivity.java
+++ app/src/main/java/chisw/com/plans/ui/MediaActivity.java
@@ -6,19 +6,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import chisw.com.plans.R;
 
 public class MediaActivity extends Activity {
+    private Button goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
+        goBack = (Button) findViewById(R.id.ma_goback_btn);
     }
 
     public static void Start(Activity activity) {
-        Intent intnt = new Intent(activity,MediaActivity.class);
+        Intent intnt = new Intent(activity, MediaActivity.class);
         activity.startActivity(intnt);
     }
 
