740f5f63ae12cc674793e4100de749377f809338
vdbo <19vladbond95@gmail.com>
2015-06-15 12:26:32 +0300

    added NetManagment Activity!

---
+++ app/src/main/java/chisw/com/plans/ui/NetManagment.java
@@ -0,0 +1,18 @@
+package chisw.com.plans.ui;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import chisw.com.plans.R;
+
+public class NetManagment extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_managment);
+    }
+}
