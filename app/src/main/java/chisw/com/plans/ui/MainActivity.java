cd12daef9dcc7896218c6f0b142a729d46b37d4f
Ivan <Kuzlo>
2015-06-15 11:07:53 +0300

    add activity

---
+++ app/src/main/java/chisw/com/plans/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package chisw.com.plans.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import chisw.com.plans.R;
+
+/**
+ * Created by Kuzlo on 15.06.2015.
+ */
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
