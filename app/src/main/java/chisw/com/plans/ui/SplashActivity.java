e299231a5c95c2d7d70611b0a29587fcccda84e3
Yuriy_Getsko <getttzzz@gmail.com>
2015-06-15 13:16:13 +0300

    Add find view by id "alarmBt"

--- app/src/main/java/chisw/com/plans/ui/SplashActivity.java
+++ app/src/main/java/chisw/com/plans/ui/SplashActivity.java
@@ -8,11 +8,18 @@ import chisw.com.plans.R;
 
 public class SplashActivity extends Activity {
     private Button mediaBt;
+    private Button alarmBt;
+    private Button plannerBt;
+    private Button settingBt;
+    private Button net_managerBt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_main);
         mediaBt = (Button) findViewById(R.id.mediaBt);
+        alarmBt = (Button) findViewById(R.id.alarmBt);
+        
     }
 
 
