5d879ba463c5bb24a446f0d5f23b983a95dd8b27
vdbo <19vladbond95@gmail.com>
2015-06-16 12:49:49 +0300

    added button and textView to NetManagmentActivity!

--- app/src/androidTest/java/chisw/com/plans/ApplicationTest.java
+++ app/src/androidTest/java/chisw/com/plans/ApplicationTest.java
@@ -1,13 +1,18 @@
 package chisw.com.plans;
 
 import android.app.Application;
+import android.content.res.Configuration;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import com.parse.Parse;
+
+
+public class ApplicationTest extends Application {
+
+    public void onCreate() {
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "fYItmKEFfg4ZxDEB1SrwVMUx82sw91XMyTeZJ0fC", "QVWnE2OAOKVt5yv3KRt830rguZv22wkk8ySkLA4K");
     }
 }
