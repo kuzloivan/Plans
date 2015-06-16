166841e43f5c7081f9d265db3a01cc7cd9c1aa8e
Alyasko <yasko.alexander@gmail.com>
2015-06-16 12:38:24 +0300

    Added: styled toolbar.

--- app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
+++ app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,16 +16,31 @@ import chisw.com.plans.R;
  */
 public class PlannerActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
 
+        initToolbar();
+
         Clicker clicker = new Clicker();
+
+
 
         findViewById(R.id.pa_goBack_btn).setOnClickListener(clicker);
 
         //getActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(R.string.title_activity_planner);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public static void start(Activity activity){
