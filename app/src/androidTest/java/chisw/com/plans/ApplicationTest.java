55864d968398794cadacd7f77c0e73672c791672
vdbo <19vladbond95@gmail.com>
2015-06-16 12:56:09 +0300

    added Parse SDK

--- app/src/androidTest/java/chisw/com/plans/ApplicationTest.java
+++ app/src/androidTest/java/chisw/com/plans/ApplicationTest.java
@@ -9,10 +9,4 @@ import com.parse.Parse;
 
 public class ApplicationTest extends Application {
 
-    public void onCreate() {
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
-
-        Parse.initialize(this, "fYItmKEFfg4ZxDEB1SrwVMUx82sw91XMyTeZJ0fC", "QVWnE2OAOKVt5yv3KRt830rguZv22wkk8ySkLA4K");
-    }
 }
--- app/src/main/java/chisw/com/plans/core/PApplication.java
+++ app/src/main/java/chisw/com/plans/core/PApplication.java
@@ -2,7 +2,15 @@ package chisw.com.plans.core;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 public class PApplication extends Application {
 
+    public void onCreate() {
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "fYItmKEFfg4ZxDEB1SrwVMUx82sw91XMyTeZJ0fC", "QVWnE2OAOKVt5yv3KRt830rguZv22wkk8ySkLA4K");
+    }
 
 }
