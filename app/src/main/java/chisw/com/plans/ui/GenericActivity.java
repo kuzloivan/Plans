380060c5154a3c0a9413ba27d73422d9df62bbc4
Alyasko <yasko.alexander@gmail.com>
2015-06-16 14:10:21 +0300

    Added: action bar returns user to previous screen.

--- app/src/main/java/chisw/com/plans/ui/GenericActivity.java
+++ app/src/main/java/chisw/com/plans/ui/GenericActivity.java
@@ -3,6 +3,8 @@ package chisw.com.plans.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import chisw.com.plans.R;
 
@@ -25,6 +27,17 @@ public abstract class GenericActivity extends AppCompatActivity {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     protected abstract int contentViewResId();
