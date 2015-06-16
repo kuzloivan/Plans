68ace96455124de8d1ff309d3b8e7369e2a61297
Alyasko <yasko.alexander@gmail.com>
2015-06-16 12:32:50 +0300

    Added: toolbar.

--- app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
+++ app/src/main/java/chisw/com/plans/ui/PlannerActivity.java
@@ -4,7 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,7 +13,7 @@ import chisw.com.plans.R;
 /**
  * Created by Alexander on 15.06.2015.
  */
-public class PlannerActivity extends ActionBarActivity {
+public class PlannerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
