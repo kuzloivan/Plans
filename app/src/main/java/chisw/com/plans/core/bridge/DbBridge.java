5bc51d928f1fcda55b584e197cdcf7d7b78435bc
andrey_kozhurov <Kbuba.v1.0@gmail.com>
2015-06-17 12:34:11 +0300

    complete db bridge

--- app/src/main/java/chisw/com/plans/core/bridge/DbBridge.java
+++ app/src/main/java/chisw/com/plans/core/bridge/DbBridge.java
@@ -1,4 +1,11 @@
 package chisw.com.plans.core.bridge;
 
+import java.util.List;
+
+import chisw.com.plans.model.Plan;
+
 public interface DbBridge {
+
+    void saveNewPlan(Plan pPlan);
+    List<Plan> getAllPlans();
 }
