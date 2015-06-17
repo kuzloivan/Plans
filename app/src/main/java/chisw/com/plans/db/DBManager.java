94f6e22688a79cc765d665ff825d38cbfa69c47c
darinal <darinka.lysenko@yandex.ua>
2015-06-17 13:23:32 +0300

    DBHelper

--- app/src/main/java/chisw/com/plans/db/DBManager.java
+++ app/src/main/java/chisw/com/plans/db/DBManager.java
@@ -1,5 +1,7 @@
 package chisw.com.plans.db;
 
+import android.content.Context;
+
 import java.util.List;
 
 import chisw.com.plans.core.bridge.DbBridge;
@@ -9,6 +11,14 @@ import chisw.com.plans.model.Plan;
  * Created by Alexander on 17.06.2015.
  */
 public class DBManager implements DbBridge {
+
+    DBHelper dbHelper;
+
+    public DBManager (Context context)    {
+        dbHelper = new DBHelper(context);
+
+    }
+
     @Override
     public void saveNewPlan(Plan pPlan) {
 
---
+++ app/src/main/java/chisw/com/plans/db/entity/PlansEntity.java
@@ -0,0 +1,21 @@
+package chisw.com.plans.db.entity;
+
+/**
+ * Created by Darina on 17.06.2015.
+ */
+public class PlansEntity {
+    public static final String TABLE_NAME = "Plans";
+    public static final String PARSE_ID = "parseId";
+    public static final String NAME = "name";
+    public static final String TIMESTAMP = "timeStamp";
+
+    public static final String CREATE_SCRIPT = "Create table " + TABLE_NAME +
+            " ( " +
+            "_id INT, " +
+            "PRIMARY KEY _id, " +
+            PARSE_ID + " TEXT, " +
+            NAME + " TEXT, " +
+            TIMESTAMP + " TEXT, " +
+            ")";
+
+}
