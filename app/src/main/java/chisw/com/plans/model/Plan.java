0b97cfc00cb6691e8626c0a521b3aae0ee4db981
Alyasko <yasko.alexander@gmail.com>
2015-06-17 12:37:18 +0300

    Added: plan class.

--- app/src/main/java/chisw/com/plans/model/Plan.java
+++ app/src/main/java/chisw/com/plans/model/Plan.java
@@ -4,4 +4,31 @@ package chisw.com.plans.model;
  * Created by Alexander on 17.06.2015.
  */
 public class Plan {
+    private String parseId;
+    private String title;
+    private long timeStamp;
+
+    public String getParseId() {
+        return parseId;
+    }
+
+    public void setParseId(String parseId) {
+        this.parseId = parseId;
+    }
+
+    public String getName() {
+        return title;
+    }
+
+    public void setName(String name) {
+        this.title = name;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
