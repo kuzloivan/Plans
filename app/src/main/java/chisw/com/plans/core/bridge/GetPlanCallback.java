7bee341029d3573a262c9c7be782c3a762ce34e3
Vladislav <Yermakov.Vladislav@yandex.ru>
2015-06-24 16:32:41 +0300

    interface getplancallback

---
+++ app/src/main/java/chisw/com/plans/core/bridge/GetPlanCallback.java
@@ -0,0 +1,10 @@
+package chisw.com.plans.core.bridge;
+
+import com.parse.ParseObject;
+
+/**
+ * Created by VladislavY on 6/24/2015.
+ */
+public interface GetPlanCallback {
+    void getPlan(ParseObject parseObject);
+}
