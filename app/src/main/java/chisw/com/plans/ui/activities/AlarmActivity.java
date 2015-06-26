e86e62a9b9fd096d64a79045511212baaee41792
Konstantin <yamkostya@gmail.com>
2015-06-26 11:37:23 +0300

    doesn't work (only mp3 supported)

--- app/src/main/java/chisw/com/plans/ui/activities/AlarmActivity.java
+++ app/src/main/java/chisw/com/plans/ui/activities/AlarmActivity.java
@@ -237,10 +237,10 @@ public class AlarmActivity extends ToolbarActivity implements DaysOfWeekDialog.D
 
         switch (requestCode) {
             case REQUEST_AUDIO_GET:
-                    if ( data.getType() != "audio/mp3"){
+                    /*if ( data.getType() != "audio/mp3"){
                         showToast("File is not valid");
                         return;
-                    }
+                    }*/
                     path = getPath(data);
                     if (SystemUtils.isKitKatHigher()) {
                         durationBuf = getAudioDuration(data.getData(), this);
