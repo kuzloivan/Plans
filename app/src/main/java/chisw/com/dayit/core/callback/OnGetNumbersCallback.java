59c3b428d2ba3469e6f3b5686ed2f9108858fb82
vdbo <19vladbond95@gmail.com>
2015-07-07 23:45:57 +0300

    Added ability to get certain numbers of users from Parse

---
+++ app/src/main/java/chisw/com/dayit/core/callback/OnGetNumbersCallback.java
@@ -0,0 +1,10 @@
+package chisw.com.dayit.core.callback;
+
+import java.util.Map;
+
+/**
+ * Created by vdboo_000 on 07.07.2015.
+ */
+public interface OnGetNumbersCallback {
+    void getNumbers(Map<String, String> numbers);
+}
